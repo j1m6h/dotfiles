644b6c362780517d37b0fa14147444acfc1e1845
Joshua <joshuamatthewhagen@yahoo.com>
2021-10-27 19:19:23 -0500

    automated setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-mv .xinitrc ~/
-mv .bashrc ~/
-mv .bash_profile ~/
-mv .config/nvim ~/.config/
-mv .config/neofetch ~/.config/
+mv_config_files() {
+	echo "Moving configuration files - this will overwrite current configuration files..."
+	if [ -f "~/.config/nvim/" ]; then
+		rm -r ~/.config/nvim
+	fi
+
+	mv .xinitrc ~/ 
+	mv .bashrc ~/ 
+	mv .bash_profile ~/
+	mv .config/nvim/init.vim ~/.config/nvim/
+}
+
+if [ "$EUID" -ne 0 ]; then
+	echo "**You must be root to install dependencies**"
+	mv_config_files
+	exit
+fi
+
+alias not_found="! command -v $1> &> /dev/null"
+install_deps() {
+	if not_found picom then
+		emerge -av x11-misc/picom
+	fi
+	if not_found feh then
+		emerge -av media-gfx/feh
+	fi
+}
+
+install_deps
+mv_config_files
