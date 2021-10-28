8ca715fbcd31ab73fa1b253bae9a3ba9551acfeb
Joshua <44485177+j1m6h@users.noreply.github.com>
2021-10-28 00:33:16 -0500

    Update monitor_orientation.sh

--- .screenlayout/monitor_orientation.sh
+++ .screenlayout/monitor_orientation.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
 
+xrandr --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP-2 --off --output DP-3 --off --output HDMI-0 --primary --mode 2560x1080 --pos 1080x551 --rotate normal --output DP-4 --off --output DP-5 --off
